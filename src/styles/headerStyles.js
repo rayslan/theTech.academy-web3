0667156b8419a78f5152a5b84bc51ec678448f83
Your Name <you@example.com>
2021-08-20 19:28:09 +0000

    qwr

--- src/styles/headerStyles.js
+++ src/styles/headerStyles.js
@@ -30,4 +30,24 @@ export const Logo = styled.div`
 
 `
 
-export const Menu = styled.div``
+export const Menu = styled.div`
+    button {
+        transform-origin: center;
+        border: none;
+        padding: 20px;
+        background: none;
+        outline: none;
+        span {
+            width: 36px;
+            height: 8px;
+            display: block;
+            background: ${props => props.theme.text};
+            margin: 8px;
+
+        }
+    }
+
+
+
+
+`
